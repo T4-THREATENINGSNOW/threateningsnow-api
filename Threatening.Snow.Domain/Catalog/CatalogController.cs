a5e201d4fc1a1c4401134ce52d92a1a9f134bf51
unknown <ishwaryaiyer3@gmail.com>
2025-03-25 17:25:24 -0400

    Part 1: Catalog API Till 4 - Post New Item

---
+++ Threatening.Snow.Domain/Catalog/CatalogController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Threatening.Snow.Domain.Catalog;
+
+namespace Threatening.Snow.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetItems()
+        {
+            var items = new List<Item>()
+            {
+                new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+                new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+            };
+
+            return Ok(items);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id)
+        {
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.Id = id;
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Item item)
+        {
+            return Created("/catalog/42", item);
+        }
+    }
+}
---
+++ Threatening.Snow.Domain/Catalog/Rating.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Threatening.Snow.Domain.Catalog
+{
+    public class Rating
+    {
+        public int Stars {get; set;}
+        public string UserName {get; set;}
+        public string Review {get; set;}
+    }
+}
