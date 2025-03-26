75197454424d508295b8884f65d9ad30f0cbed52
Sean Moore <seanmoorena19@gmail.com>
2025-03-26 15:16:27 -0400

    Finish setup for EF Core and exclude database files

--- Threatening.Snow.Domain/Catalog/Rating.cs
+++ Threatening.Snow.Domain/Catalog/Rating.cs
@@ -4,6 +4,7 @@ namespace Threatening.Snow.Domain.Catalog
 {
     public class Rating
     {
+        public int Id { get; set; } 
         public int Stars {get; set;}
         public string UserName {get; set;}
         public string Review {get; set;}
