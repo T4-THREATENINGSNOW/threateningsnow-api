58da42b33ed27accee11d86b8c862e320c17418f
parinajpatel <parinapatel4@gmail.com>
2025-03-26 19:20:31 -0400

    API now returns data from the database

--- Threatening.Snow.Data/StoreContext.cs
+++ Threatening.Snow.Data/StoreContext.cs
@@ -5,11 +5,16 @@ namespace Threatening.Snow.Data
 {
     public class StoreContext : DbContext
     {
-        public StoreContext() { }
-
-        public StoreContext(DbContextOptions<StoreContext> options)
-            : base(options) { }
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
 
         public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            DbInitializer.Initialize(modelBuilder);
+        }
     }
 }
