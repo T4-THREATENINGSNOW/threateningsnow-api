a0e7b20d4971b01cd56f94a431dcc54a6d86794e
parinajpatel <parinapatel4@gmail.com>
2025-04-05 21:55:26 -0400

    Intial Commit PP. 3

---
+++ tests/threatening.snow.api.Domain.Tests/RatingTests.cs
@@ -0,0 +1,21 @@
+﻿using _threatening.snow.Domain
+using _threatening.snow.Domain.Catalog
+namespace _threatening.snow.api_.Domain.Tests;
+
+[TestClass]
+public sealed class RatingTests
+{
+    [TestMethod]
+    public void Can_Create_New_Rating()
+    {
+        // Arrange
+        var rating = new Rating(1, "Mike", "Great fit!");
+
+        // Act (empty)
+
+        // Assert
+        Assert.AreEqual(1, rating.Stars);
+        Assert.AreEqual("Mike", rating.UserName);
+        Assert.AreEqual("Great fit!", rating.Review);
+    }
+}
